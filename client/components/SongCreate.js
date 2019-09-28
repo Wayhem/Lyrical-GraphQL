ec03916cd137ce32a7d14ad99741af4b938f218a
Oscar Villarroel <oscar.villarroel@clouddistrict.com>
2019-09-28 22:00:05 +0200

    soon to test some stuff

---
+++ client/components/SongCreate.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+
+const SongCreate = () => {
+  return (
+    <Fragment>
+      <h3>create new song</h3>
+      <form></form>
+    </Fragment>
+  );
+};
+
+export default SongCreate;
--- client/index.js
+++ client/index.js
@@ -2,15 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import ApolloClient from "apollo-client";
 import { ApolloProvider } from "react-apollo";
+import { Route, Router, Switch } from "react-router";
 
 import SongList from "./components/SongList";
+import SongCreate from "./components/SongCreate";
+import { history } from "./history";
 
 const client = new ApolloClient({});
 
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <SongList />
+      <Router history={history}>
+        <Switch>
+          <Route path="/" component={SongList} />
+          <Route path="/songs/new" component={SongCreate} />
+        </Switch>
+      </Router>
     </ApolloProvider>
   );
 };
