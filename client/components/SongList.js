b5911b61b119714064d8ec2e88f91be7dbe80551
Oscar Villarroel <oscar.villarroel@clouddistrict.com>
2019-09-28 19:31:44 +0200

    component using graphQL

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,13 +1,19 @@
 import React from "react";
 import gql from "graphql-tag";
 import { graphql } from "react-apollo";
+import _ from "lodash";
 
-const SongList = props => <div>{console.log(props)}hi</div>;
-
+const SongList = ({ data }) => {
+  const { songs, loading } = data;
+  const renderSongs = () =>
+    _.map(songs, ({ title, id }) => <li key={id}>{title}</li>);
+  return <div>{loading ? <div>loading</div> : renderSongs()}</div>;
+};
 const query = gql`
   {
     songs {
       title
+      id
     }
   }
 `;
